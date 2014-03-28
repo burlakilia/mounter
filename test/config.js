e8908f9b6e5cfacc64ea3148a68a85cba15aa9e3
Burlak Ilia <i.burlak@it-online.ru>
2014-03-28 17:23:27 +0400

    Fix test

--- test/config.js
+++ test/config.js
@@ -2,7 +2,7 @@ exports['config should have correct endpoint for private git endpoint'] = functi
     var config = require('../src/utils/conf').load(__dirname + '/misc/mounter')['app'];
 
     test.ok(typeof config.endpoint === 'object', 'have endpoint object')
-    test.ok(config.endpoint.version === '0.0.1', 'correct version');
+    test.ok(config.endpoint.version === '0.0.x', 'correct version');
     test.ok(config.endpoint.repository === 'git@git.dev:itoapp.git', 'correct repository');
     test.ok(config.endpoint.vcs === 'git', 'correct vcs');
     test.done();
@@ -10,8 +10,6 @@ exports['config should have correct endpoint for private git endpoint'] = functi
 
 exports['config should have correct endpoint for github'] = function(test) {
     var config = require('../src/utils/conf').load(__dirname + '/misc/mounter')['jquery'];
-
-    console.log(config);
 
     test.ok(typeof config.endpoint === 'object', 'have endpoint object')
     test.ok(config.endpoint.version === '1.9.x', 'correct version');
